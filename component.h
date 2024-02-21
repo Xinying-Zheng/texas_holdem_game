859be3a6aa1785a9d85cfe5682bd630de08369d5
Xinying-Zheng <miaozi1004@gmail.com>
2024-02-21 22:23:53 +0800

    framework version 1

---
+++ component.h
@@ -0,0 +1,26 @@
+#include<vector>
+using namespace std;
+
+class Poker {
+};
+
+class Dealer {
+};
+
+class Player {
+};
+
+class Table {
+};
+
+class Chip {
+};
+
+class Game {
+	int player_num;
+	int chip_num;
+	vector<Player> players;
+	Poker poker;
+	Dealer dealer;
+	Table table;
+};
