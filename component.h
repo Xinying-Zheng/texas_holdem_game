bf88340438d559ddba70e29cb21ecec6f0450ed2
Xinying-Zheng <miaozi1004@gmail.com>
2024-02-21 23:41:06 +0800

    add class poker

--- component.h
+++ component.h
@@ -2,15 +2,35 @@
 using namespace std;
 
 class Poker {
+public:
+	vector<pair<int, int>> cards;
+	int top_card_ind;
+	Poker(); // initialize
+	void shuffle();
+	pair<int, int> deal();
 };
 
 class Dealer {
+public:
+	int speak_order;
+	int judge();
+	void next_order();
 };
 
 class Player {
+public:
+	int chip_num;
+	vector<pair<int,int>> cards;
+	void fall();
+	void check();
+	void raise();
 };
 
 class Table {
+public:
+	vector<pair<int,int>> cards;
+	int cur_chip;
+	int cur_chip_sum;
 };
 
 class Chip {
--- poker.cpp
+++ poker.cpp
@@ -1,1 +1,21 @@
 #include "component.h"
+#include <algorithm>
+#include <random>
+#include <time.h>
+
+Poker::Poker() {
+	for (int i = 1; i < 5; i++) {
+		for (int j = 1; j < 14; j++) cards.push_back(make_pair(j, i));
+	}
+	top_card_ind = 0;
+}
+
+void Poker::shuffle() {
+	default_random_engine e(time(0));
+	::shuffle(cards.begin(), cards.end(), e);     
+}
+
+pair<int,int> Poker::deal() {
+	return cards[top_card_ind++];
+}
+
